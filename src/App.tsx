e9402b65bd75285d2b106fe1acfc0462b62aab52
pepeyen <erickfcaldeira@gmail.com>
2021-01-14 16:06:15 -0300

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 //Components
 import { CepBlock } from './components';
 
 const App: React.FC = () => {
 	const cepBlocksCount = 5;
-
-	useEffect(() => {
-		fetch('https://viacep.com.br/ws/31060350/json/ ')
-		.then(result => {
-			return result.json();
-		})
-		.then(data => {
-			console.log(data);
-		})
-		.catch(error => {
-			console.log(error);
-		})
-	});
-
 	return(
 		<React.Fragment>
 			{

2a88f85c44ec3ef31ec9500939b141b879b1c6bc
pepeyen <erickfcaldeira@gmail.com>
2021-01-14 02:27:12 -0300

    Implemented CepBlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import { CepBlock } from './components';
+
+const App: React.FC = () => {
+	const cepBlocksCount = 5;
+
+	useEffect(() => {
+		fetch('https://viacep.com.br/ws/31060350/json/ ')
+		.then(result => {
+			return result.json();
+		})
+		.then(data => {
+			console.log(data);
+		})
+		.catch(error => {
+			console.log(error);
+		})
+	});
+
+	return(
+		<React.Fragment>
+			{
+				[...Array(cepBlocksCount)].map((value, index: number) => {
+					return(
+						<CepBlock
+							key={index}
+							cepBlockID={index}
+						/>
+					);
+				})
+			}
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore , compose } from 'redux'
+
+//Reducers
+import {allReducers} from './reducers';
 
 //Components
 import App from './App';
@@ -10,13 +15,24 @@ import {ScrollToTop} from './components';
 //Styles
 import './assets/sass/main.scss';
 
+
+declare global {
+	interface Window {
+	  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+	}
+  }
+  
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const myStore = createStore(allReducers, composeEnhancers());
+
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={myStore}>
 		<HashRouter>
 			<ScrollToTop />
 			<App />
 		</HashRouter>
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root')
 );
 

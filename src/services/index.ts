b8d51ef969ba20ae40a4dd162c8c81e2643ee689
pepeyen <erickfcaldeira@gmail.com>
2021-01-14 16:04:16 -0300

    Introduced array item removal service

--- src/services/index.ts
+++ src/services/index.ts
@@ -11,3 +11,25 @@ export const cepConverter = (cep: string): string => {
 
     return cep;
 };
+
+export const removeFromArray = (targetArray: any[], targetValue: any) => {
+    for(let i = 0; i < targetArray.length; i++){   
+        if(targetArray[i] === targetValue) {
+            targetArray.splice(i, 1); 
+
+            i--; 
+        }else{
+            if(typeof targetArray[i] === 'object'){
+                Object.values(targetArray[i]).forEach(value => {
+                    if(value === targetValue){
+                        targetArray.splice(i, 1); 
+
+                        i--;
+                    }
+                })
+            }
+        }
+    }
+
+    return targetArray;
+};

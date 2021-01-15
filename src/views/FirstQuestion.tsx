485ab529ab26792fdd99667fd1676223d8ac330b
pepeyen <erickfcaldeira@gmail.com>
2021-01-15 03:08:58 -0300

    Introduced question views and update the file structure

---
+++ src/views/FirstQuestion.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FirstQuestion: React.FC = () => {
+    return(
+        <article>
+        </article>
+    );
+};
+
+export default FirstQuestion;
---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return(
+        <article>
+            <h1>Home</h1>
+        </article>
+    );
+};
+
+export default Home;

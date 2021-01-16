65341ff33dd9622a364664362e96e3a4aad49c19
pepeyen <erickfcaldeira@gmail.com>
2021-01-16 00:38:07 -0300

    Minor Navbar component update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+//Components
+import {
+    NavbarGroup,
+    NavbarLogo,
+    NavbarRedirector
+} from './index';
 
 const Navbar: React.FC = () => {
+    const location  = useLocation();
+
     return(
-        <nav>
-            <Link to="/questoes">Questões</Link>
-            <Link to="/questoes/primeira">Primeira Questão</Link>
-            <Link to="/questoes/segunda">Segunda Questão</Link>
-            <Link to="/questoes/terceira">Terceira Questão</Link>
-            <Link to="/questoes/quarta">Quarta Questão</Link>
+        <nav className="navbar">
+            <NavbarLogo
+                redirectTo="/questoes"
+                logoType="text"
+                horizontalAlignment="right"
+                isActive={location.pathname === "/questoes"}
+            >
+                Questões
+            </NavbarLogo>
+            <NavbarGroup horizontalAlignment="left">
+                <NavbarRedirector
+                    redirectTo="/questoes/primeira"
+                    isActive={location.pathname === "/questoes/primeira"}
+                >
+                    Primeira Questão
+                </NavbarRedirector>
+                <NavbarRedirector
+                    redirectTo="/questoes/segunda"
+                    isActive={location.pathname === "/questoes/segunda"}
+                >
+                    Segunda Questão
+                </NavbarRedirector>
+                <NavbarRedirector
+                    redirectTo="/questoes/terceira"
+                    isActive={location.pathname === "/questoes/terceira"}
+                >
+                    Terceira Questão
+                </NavbarRedirector>
+                <NavbarRedirector
+                    redirectTo="/questoes/quarta"
+                    isActive={location.pathname === "/questoes/quarta"}
+                >
+                    Quarta Questão
+                </NavbarRedirector>
+            </NavbarGroup>
         </nav>
     );
 };
